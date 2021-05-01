17347695f25f6e21db87e251fe3d0585d5171a9c
VP <VP>
2021-05-01 21:33:59 +0300

    Initial Commit

---
+++ WhatsNewScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsNewScreen
+//
+//  Created by valentine on 01.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
