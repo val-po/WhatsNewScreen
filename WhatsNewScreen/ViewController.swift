1c2323b515872a471060eec62b0cae226ddd40a4
VP <VP>
2021-05-01 22:21:19 +0300

    Commit #1

--- WhatsNewScreen/ViewController.swift
+++ WhatsNewScreen/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import WhatsNewKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let whatsNew = WhatsNew(title: "What`s New",
+                              items: [
+                                WhatsNew.Item(title: "Add Favorites",
+                                              subtitle: "Now you can add favorites in the app!",
+                                              image: UIImage(systemName: "star")),
+                                WhatsNew.Item(title: "Volume",
+                                              subtitle: "You can controlthe volume now!",
+                                              image: UIImage(systemName: "volume")),
+                                WhatsNew.Item(title: "Listen",
+                                              subtitle: "Listen to the top songs around the world!",
+                                              image: UIImage(systemName: "ear")),
+                                WhatsNew.Item(title: "Settings",
+                                              subtitle: "Control the quality of your music!",
+                                              image: UIImage(systemName: "gear"))
+                              ])
+        
+        let vc = WhatsNewViewController(whatsNew: whatsNew, theme: .whiteRed)
+        
+        present(vc, animated: true)
+    }
 
 }
 
